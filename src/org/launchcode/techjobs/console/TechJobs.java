2944a1080d07b77c9f885df808560f27d90b9753
setite <setyte@gmail.com>
2018-11-11 19:33:42 -0600

    implemented PrintJob with nice even spacing

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        // System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() > 0) {
+            for(HashMap<String, String> job : someJobs) {
+                System.out.println("******");
+                for(String key : job.keySet()) {
+//                    System.out.println(key + ": " + job.get(key));
+                    System.out.printf("%15s : %-20s \n", key , job.get(key));
+                }
+                System.out.println("******");
+            }
+        }
+        else {
+            System.out.println("No jobs found.");
+        }
     }
 }
